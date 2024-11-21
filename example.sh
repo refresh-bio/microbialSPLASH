cd9f831165aef666d7fd6a34441f85d686b735f2
agudys <adam.gudys@gmail.com>
2024-11-21 09:59:56 +0100

    UniProt sequences added

--- example.sh
+++ example.sh
@@ -11,7 +11,10 @@
 # create list of R1 FASTQ files
 ls ./input/samples/*_1.fastq > ./input/samples.list
 
-# run the pipeline against uniprotkb_NOT_Eukaryota_AND_reviewed.fasta sequence database
+# concatenate partial files into single one
+cat ./input/uniprot/*.fa > ./input/uniprotkb_not_eukaryota.fa
+
+# run the pipeline against non-eukariotic UniProtKbB sequences
 ./assemble_and_align.py \
 	--positive_index_list ./input/lookup/positive_index.list \
 	--positive_index_k 23 \
@@ -19,13 +22,13 @@ ls ./input/samples/*_1.fastq > ./input/samples.list
 	--negative_index_k 18 \
 	--out_dir ./out-align \
 	./input/samples.list \
-	./input/uniprotkb_NOT_Eukaryota_AND_reviewed.fasta \
+	./input/uniprotkb_not_eukaryota.fa \
 	
 # extract and analyze CAS-targeted hits	
 ./extract_cas.py \
 	--out_dir out-cas \
 	./input/samples.list \
-	./input/uniprotkb_NOT_Eukaryota_AND_reviewed.fasta \
+	./input/uniprotkb_not_eukaryota.fa \
 	./out-align/alignments.tsv \
 	./out-align/compactors.fasta \
 	
